aef59c72c3971be1f5ea7a5555f7a71a29185d17
CN02233 <black41802935@163.com>
2017-07-18 11:12:23 +0800

    add table css,user manage

--- spider-show/web/template/sys/user_mg/js/user_mg_main.js
+++ spider-show/web/template/sys/user_mg/js/user_mg_main.js
@@ -9,5 +9,26 @@ $(document).ready(function(){
 });
 
 function page_callback(dataList){
-    // console.log("call back function is running...."+JSON.stringify(dataList));
+    console.log("call back function is running...."+JSON.stringify(dataList));
+    var $user_table = $("#user_table").find("tbody");
+    $user_table.empty();
+    $.each(dataList,function(i,dataObj){
+        var user_id = dataObj["user_id"];
+        var user_name = dataObj["user_name"];
+        var user_type = dataObj["user_type"];
+        var reg_date =  dataObj["reg_date"];
+        var user_status =  dataObj["user_status"];
+        var last_login_time = dataObj["last_login_time"];
+
+        var $tr = $("<tr></tr>");
+        $tr.append("<td>"+user_id+"</td>");
+        $tr.append("<td>"+user_name+"</td>");
+        $tr.append("<td>"+user_type+"</td>");
+        $tr.append("<td>"+reg_date+"</td>");
+        $tr.append("<td>"+user_status+"</td>");
+        $tr.append("<td>"+last_login_time+"</td>");
+        $tr.append("<td>操作栏</td>");
+
+        $user_table.append($tr);
+    });
 }
